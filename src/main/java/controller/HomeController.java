efb2896ac0aa3fefd3088a8392debc67ddbcb593
nhwdev <nhw.dev@gmail.com>
2026-04-16 16:49:55 +0900

    feat: HomeController 추가

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,13 +2,15 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("home")
 public class HomeController {
 
     @RequestMapping("*")
-    public String home() {
-        return "home";
+    public ModelAndView home() {
+        ModelAndView mav = new ModelAndView();
+        return mav;
     }
 }

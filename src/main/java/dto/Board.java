39886d026292608cd487a5f837ed9df753d6ec4b
nhwdev <nhw.dev@gmail.com>
2026-04-20 16:22:30 +0900

    style: 레이아웃 구조 개선 및 CSS 수정

--- src/main/java/dto/Board.java
+++ src/main/java/dto/Board.java
@@ -1,23 +1,25 @@
 package dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
-    private int board_no; // 게시판 관리 번호
-    private int writer_no; // 작성자 관리 번호
-    private int course_no; // 강의 관리 번호
-    private String board_type; // 공지, 자유, Q&A
-    private String title; // 게시글 제목
-    private String content; // 게시글 내용
-    private String file_url; // 첨부파일 경로
-    private int views; // 조회수
-    private int is_secret; // 비밀글 여부 (Q&A)
-    private Date created_at; // 게시글 생성 날짜
+
+    private int boardNo;             // 게시판 관리 번호 (AUTO_INCREMENT)
+    private Integer courseNo;        // 강의 관리 번호 (nullable)
+    private Integer writerNo;        // 작성자 번호
+    private String boardType;        // 공지, 자유, Q&A (ENUM)
+    private String title;            // 게시글 제목
+    private String content;          // 게시글 내용
+    private String fileUrl;          // 첨부파일 경로 (nullable)
+    private int views;               // 조회수 (기본값 0)
+    private int isSecret;            // 비밀글 여부 (기본값 0)
+    private LocalDateTime createdAt; // 게시글 생성 일자
+
 }

29f7a853f74ada3fd85af2ef55e3783bf0a7054d
nhwdev <nhw.dev@gmail.com>
2026-04-20 12:35:33 +0900

    style: 레이아웃 구조 개선 및 CSS 모듈화

---
+++ src/main/java/sitemesh/SitemeshFilter.java
@@ -0,0 +1,20 @@
+package sitemesh;
+
+import javax.servlet.annotation.WebFilter;
+
+import org.sitemesh.builder.SiteMeshFilterBuilder;
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
+
+@WebFilter("/*")
+public class SitemeshFilter extends ConfigurableSiteMeshFilter{
+	// Connection Pool
+    @Override
+    protected void applyCustomConfiguration(SiteMeshFilterBuilder builder){
+        builder.addDecoratorPath("/*", "layout.jsp")
+                .addExcludedPath("/ajax/*") // 요청 url이 /ajax/ 인 모든 요청에 대해서 layout 적용 안하도록 설정
+                .addExcludedPath("/user/idsearch*")
+                .addExcludedPath("/user/pwsearch*")
+                .addExcludedPath("/home/home");
+    }
+    
+}

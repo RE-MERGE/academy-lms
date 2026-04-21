15dc736584cb15c205f007802266e4b0d16de0a8
jbo219 <nokcab1234@naver.com>
2026-04-21 16:39:11 +0900

    feat: 수강신청 첫 페이지 구현, 강의 개설 폼 구현

--- src/main/java/service/BoardService.java
+++ src/main/java/service/BoardService.java
@@ -1,6 +1,7 @@
 package service;
 
 import dao.BoardDao;
+import dto.Course;
 import dto.board.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
